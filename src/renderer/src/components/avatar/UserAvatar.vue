bb0cdd7dc437f7b20c9a561b20e18d8c361d4308
jjfang7 <jjfang7@iflytek.com>
2023-12-15 17:10:59 +0800

    Some optimization

--- src/renderer/src/components/avatar/UserAvatar.vue
+++ src/renderer/src/components/avatar/UserAvatar.vue
@@ -48,69 +48,74 @@ const selectImageRequest = (option: RequestOption) => {
 </script>
 
 <template>
-  <div>
-    <a-avatar
-      shape="square"
-      :size="size"
-      @click="modalVisible = !systemStore.chatWindowLoading && editable"
+  <a-avatar
+    class="user-avatar"
+    shape="square"
+    :size="size"
+    @click="modalVisible = !systemStore.chatWindowLoading && editable"
+  >
+    <img v-if="userStore.avatar" :src="'file://' + userStore.avatar" alt="" />
+    <icon-user v-else />
+  </a-avatar>
+  <!-- 用户设置Modal -->
+  <a-modal
+    v-model:visible="modalVisible"
+    :footer="false"
+    unmount-on-close
+    title-align="start"
+    width="350px"
+  >
+    <template #title> {{ $t('userSetting.name') }} </template>
+    <div
+      style="
+        height: 200px;
+        overflow-y: auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 15px;
+      "
     >
-      <img v-if="userStore.avatar" :src="'file://' + userStore.avatar" />
-      <icon-user v-else />
-    </a-avatar>
-    <!-- 用户设置Modal -->
-    <a-modal
-      v-model:visible="modalVisible"
-      :footer="false"
-      unmount-on-close
-      title-align="start"
-      width="350px"
-    >
-      <template #title> {{ $t('userSetting.name') }} </template>
-      <div
-        style="
-          height: 200px;
-          overflow-y: auto;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          gap: 15px;
-        "
+      <a-upload
+        :file-list="avatarFile ? [avatarFile] : []"
+        :show-file-list="false"
+        :custom-request="selectImageRequest"
+        accept="image/*"
       >
-        <a-upload
-          :file-list="avatarFile ? [avatarFile] : []"
-          :show-file-list="false"
-          :custom-request="selectImageRequest"
-          accept="image/*"
-        >
-          <template #upload-button>
-            <div class="arco-upload-list-item">
-              <div
-                v-if="avatarFile && avatarFile.url"
-                class="arco-upload-list-picture custom-upload-avatar"
-              >
-                <img :src="'file://' + avatarFile.url" />
-                <div class="arco-upload-list-picture-mask">
-                  <IconEdit />
-                </div>
+        <template #upload-button>
+          <div class="arco-upload-list-item">
+            <div
+              v-if="avatarFile && avatarFile.url"
+              class="arco-upload-list-picture custom-upload-avatar"
+            >
+              <img :src="'file://' + avatarFile.url" alt="" />
+              <div class="arco-upload-list-picture-mask">
+                <IconEdit />
               </div>
-              <div v-else class="arco-upload-picture-card">
-                <div class="arco-upload-picture-card-text">
-                  <IconPlus />
-                  <div style="margin-top: 10px; font-weight: 600">
-                    {{ $t('userSetting.selectAvatar') }}
-                  </div>
+            </div>
+            <div v-else class="arco-upload-picture-card">
+              <div class="arco-upload-picture-card-text">
+                <IconPlus />
+                <div style="margin-top: 10px; font-weight: 600">
+                  {{ $t('userSetting.selectAvatar') }}
                 </div>
               </div>
             </div>
-          </template>
-        </a-upload>
-        <a-space direction="horizontal" :size="10" fill>
-          <div>{{ $t('userSetting.nickname') }}</div>
-          <a-input v-model="userStore.nickname" size="small" />
-        </a-space>
-      </div>
-    </a-modal>
-  </div>
+          </div>
+        </template>
+      </a-upload>
+      <a-space direction="horizontal" :size="10" fill>
+        <div>{{ $t('userSetting.nickname') }}</div>
+        <a-input v-model="userStore.nickname" size="small" />
+      </a-space>
+    </div>
+  </a-modal>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.user-avatar {
+  :deep(.arco-avatar-image) {
+    background-color: var(--color-bg-white);
+  }
+}
+</style>
