76734acc1a7a2d5633268d0bbb1d5e7921d6bcbb
Junki <fangjunjievip@hotmail.com>
2023-12-22 14:28:25 +0800

    Some optimization

--- src/renderer/src/components/views/knowledge-base/knowledge-base-window/KnowledgeBaseWindow.vue
+++ src/renderer/src/components/views/knowledge-base/knowledge-base-window/KnowledgeBaseWindow.vue
@@ -3,19 +3,43 @@ import { reactive, toRefs } from 'vue'
 import { useKnowledgeBaseStore } from '@renderer/store/knowledge-base'
 import KnowledgeBaseWindowHeader from '@renderer/components/views/knowledge-base/knowledge-base-window/KnowledgeBaseWindowHeader.vue'
 
+// store
 const knowledgeBaseStore = useKnowledgeBaseStore()
 
+// 数据绑定
 const data = reactive({
   // 当前的助手
-  currentKnowledgeBase: knowledgeBaseStore.getCurrentKnowledgeBase
+  currentKnowledgeBase: knowledgeBaseStore.getCurrentKnowledgeBase,
+  // 问题
+  question: ''
 })
-const { currentKnowledgeBase } = toRefs(data)
+const { currentKnowledgeBase, question } = toRefs(data)
+
+// 提问
+const sendQuestion = () => {
+  console.log(data.question)
+}
 </script>
 
 <template>
   <div class="knowledge-base-window">
     <!-- 头部 -->
     <KnowledgeBaseWindowHeader :current-knowledge-base="currentKnowledgeBase" />
+    <!-- 主体 -->
+    <div class="knowledge-base-search-result"></div>
+    <div class="knowledge-base-search-input">
+      <a-input
+        v-model="question"
+        size="large"
+        :placeholder="$t('knowledgeBase.window.search')"
+        class="search-input"
+        @press-enter="sendQuestion"
+      >
+        <template #suffix>
+          <icon-search style="cursor: pointer" @click="sendQuestion" />
+        </template>
+      </a-input>
+    </div>
   </div>
 </template>
 
@@ -26,5 +50,21 @@ const { currentKnowledgeBase } = toRefs(data)
   overflow: hidden;
   display: flex;
   flex-direction: column;
+
+  .knowledge-base-search-result {
+    flex: 1;
+    min-height: 0;
+  }
+
+  .knowledge-base-search-input {
+    flex-shrink: 0;
+    box-sizing: border-box;
+    padding: 20px;
+
+    .search-input {
+      border: none;
+      background-color: var(--color-fill-2);
+    }
+  }
 }
 </style>
