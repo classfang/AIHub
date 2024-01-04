02cec7f362a2a1c6382f22438328399628f81dfb
Junki <fangjunjievip@hotmail.com>
2024-01-04 16:53:23 +0800

    Some optimization

--- src/renderer/src/utils/big-model/tongyi-util.ts
+++ src/renderer/src/utils/big-model/tongyi-util.ts
@@ -9,6 +9,7 @@ export const getTongyiChatUrl = (model: string) => {
     case 'qwen-turbo':
     case 'qwen-plus':
     case 'qwen-max':
+    case 'qwen-max-longcontext':
       return 'https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation'
     case 'qwen-vl-plus':
       return 'https://dashscope.aliyuncs.com/api/v1/services/aigc/multimodal-generation/generation'
