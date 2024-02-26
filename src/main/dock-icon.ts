afb02c445da7994dd42b08e38be164ef53f86626
Junki <fangjunjievip@hotmail.com>
2024-02-26 14:50:02 +0800

    优化

--- src/main/dock-icon.ts
+++ src/main/dock-icon.ts
@@ -1,4 +1,3 @@
-import icon from '../../resources/icon.png?asset'
 import icon10deg from '../../resources/icon-deg/icon-10deg.png?asset'
 import icon20deg from '../../resources/icon-deg/icon-20deg.png?asset'
 import icon30deg from '../../resources/icon-deg/icon-30deg.png?asset'
@@ -10,6 +9,7 @@ import icon80deg from '../../resources/icon-deg/icon-80deg.png?asset'
 import icon90deg from '../../resources/icon-deg/icon-90deg.png?asset'
 import icon100deg from '../../resources/icon-deg/icon-100deg.png?asset'
 import icon110deg from '../../resources/icon-deg/icon-110deg.png?asset'
+import icon from '../../resources/icon.png?asset'
 
 // 图标列表
 const iconArray = [
