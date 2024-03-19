3185316314b13b452d7d71589c0fb800a2d4c0d1
Junki <fangjunjievip@hotmail.com>
2024-03-19 13:46:29 +0800

    优化

--- src/renderer/src/components/views/ai-drawing/AIDrawingImgCurrent.vue
+++ src/renderer/src/components/views/ai-drawing/AIDrawingImgCurrent.vue
@@ -10,29 +10,29 @@ const drawingStore = useDrawingStore()
 
 <template>
   <div class="ai-drawing-img-current">
-    <a-image
-      v-if="drawingStore.getCurrentTask.imageList[0]"
-      width="500"
-      height="500"
-      :src="`file://${drawingStore.getCurrentTask.imageList[0]}`"
-      show-loader
-      fit="cover"
-    >
-      <template #preview-actions>
-        <a-image-preview-action
-          :name="$t('common.download')"
-          @click="
-            downloadFile(
-              `file://${drawingStore.getCurrentTask.imageList[0]}`,
-              `img-${drawingStore.getCurrentTask.id}.png`
-            )
-          "
-          ><icon-download
-        /></a-image-preview-action>
-      </template>
-    </a-image>
-    <a-spin v-else :loading="systemStore.aiDrawingLoading" tip="">
-      <div class="ai-drawing-img-default"></div>
+    <a-spin :loading="systemStore.aiDrawingLoading" tip="">
+      <a-image
+        v-if="drawingStore.getCurrentTask.imageList[0]"
+        width="500"
+        height="500"
+        :src="`file://${drawingStore.getCurrentTask.imageList[0]}`"
+        show-loader
+        fit="cover"
+      >
+        <template #preview-actions>
+          <a-image-preview-action
+            :name="$t('common.download')"
+            @click="
+              downloadFile(
+                `file://${drawingStore.getCurrentTask.imageList[0]}`,
+                `img-${drawingStore.getCurrentTask.id}.png`
+              )
+            "
+            ><icon-download
+          /></a-image-preview-action>
+        </template>
+      </a-image>
+      <div v-else class="ai-drawing-img-default"></div>
     </a-spin>
   </div>
 </template>
