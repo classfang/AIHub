ccdd065ca347aac3804fc74c7143396f2cecc526
Junki <fangjunjievip@hotmail.com>
2024-03-28 12:56:42 +0800

    收藏笔记功能

--- src/renderer/src/components/views/collection-set/NoteEditor.vue
+++ src/renderer/src/components/views/collection-set/NoteEditor.vue
@@ -1,10 +1,34 @@
 <script setup lang="ts">
+import { nextTick } from 'vue'
+
+// 绑定输入内容
 const content = defineModel<string>('content', { default: () => '' })
+
+// 监听输入
+const noteEditorInput = (newValue: string) => {
+  // 换行操作
+  if (newValue === content.value + '\n') {
+    const lines = content.value.split('\n')
+
+    // 序号开头
+    const matchResult = lines[lines.length - 1].match(/^\d+\.\s/)
+    if (matchResult) {
+      // 追加序号
+      nextTick(() => {
+        content.value = `${newValue}${Number(matchResult[0].replace('. ', '')) + 1}. `
+      })
+    }
+  }
+}
 </script>
 
 <template>
-  <div class="note-editor">
-    <a-textarea v-model="content" :placeholder="$t('collectionSet.note.contentPlaceholder')" />
+  <div id="note-editor" class="note-editor">
+    <a-textarea
+      v-model="content"
+      :placeholder="$t('collectionSet.note.contentPlaceholder')"
+      @input="noteEditorInput"
+    />
   </div>
 </template>
 
